3dd4cc38f5707f557fbbcf4551f24ea9d2806e68
vincent@macbook <vincent@macbook.air>
2020-09-29 23:28:46 +0800

    配置上传下载与导入导出

--- deploy/web/config.js
+++ deploy/web/config.js
@@ -1,6 +1,5 @@
 window.MC = {
-  //HOST: window.location.protocol + '//' + window.location.hostname + ':' + window.location.port,
-  //HOST: 'http://129.204.184.153:80',
+  HOST: '',
   LOGO_TEXT: '',
   LOGO: '',
 };
