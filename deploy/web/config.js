ad304c5f111206ed9e13014389994e0edb037c97
zxchengb <zxchengb@163.com>
2020-09-29 10:26:51 +0800

    Add config directory.

---
+++ deploy/web/config.js
@@ -0,0 +1,7 @@
+window.MC = {
+  //HOST: window.location.protocol + '//' + window.location.hostname + ':' + window.location.port,
+  //HOST: 'http://129.204.184.153:80',
+  LOGO_TEXT: '',
+  LOGO: '',
+};
+
